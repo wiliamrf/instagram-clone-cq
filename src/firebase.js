5496800b893dbf6faf1350c12b38475a1bc044ae
WiliamRF <wiliam.freitas@live.com>
2024-08-18 13:17:03 -0300

    1#

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+//import firebase from '../node_modules/firebase';
+import firebase from "firebase";
+
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyBRaboYAovwALPoDWK4eag7ttyZrHsijjo",
+    authDomain: "instagram-clone-cq.firebaseapp.com",
+    projectId: "instagram-clone-cq",
+    storageBucket: "instagram-clone-cq.appspot.com",
+    messagingSenderId: "501714729356",
+    appId: "1:501714729356:web:d18b11d25afd3abf40bea9",
+    measurementId: "G-3NSQRCB7YE"
+});
+
+
+
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+const functions = firebase.functions();
+
+export { db, auth, storage, functions };
