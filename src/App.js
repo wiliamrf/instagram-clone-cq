259394d49b897af02e7c82b03781bf29dd79b02b
WiliamRF <wiliam.freitas@live.com>
2024-09-29 17:41:04 -0300

    3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { db } from './firebase';
+import './firebase.js';
+import { db } from './firebase'; 
 import { useEffect, useState } from 'react';
 import Header from './Header';
 
@@ -8,12 +9,18 @@ import Header from './Header';
 
 function App() {
 
-  const [user, setUser] = useState(null);  
+  const [user, setUser] = useState();  
+
+   const [posts,setPosts] = useState([]);
 
   useEffect(() => {
 
+    db.collection('posts').orderBy('timestamp','desc').onSnapshot(function(snapshot){
 
-  }, [])
+
+    })
+
+  },[])
 
   return (
     <div className="App">
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 //import firebase from '../node_modules/firebase';
-import firebase from "firebase";
+import firebase from 'firebase';
 
 
 const firebaseApp = firebase.initializeApp({
